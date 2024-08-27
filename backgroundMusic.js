f592acb0716b4ad77df44d7d3829831e50420ea6
Lucasash <summonedskye@gmail.com>
2024-08-27 19:33:00 -0400

    Better symbol for mute

--- backgroundMusic.js
+++ backgroundMusic.js
@@ -3,7 +3,7 @@ var muteButton = document.getElementById('mute-button');
 function toggleMute() {
     if (audio.muted) {
         audio.muted = false;
-        muteButton.className = 'fa fa-deaf fa-2x'; // Image for unmuted state
+        muteButton.className = 'fa fa-volume-mute fa-2x'; // Image for unmuted state
     } else {
         audio.muted = true;
         muteButton.className = 'fa fa-music fa-2x'; // Image for muted state
